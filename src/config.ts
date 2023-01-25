5378b5c6ca7a257b7f0d91531cb1a668509f7f89
skinnyfads <skinnyfads@proton.me>
2023-01-26 06:29:07 +0700

    fix(api): rewrite config.json to config.ts

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+const config = {
+  totalTeams: {
+    1: 16,
+    2: 18,
+    3: 19,
+    default: 17,
+  },
+  degradation: {
+    default: 3,
+  },
+  startYear: 1804,
+};
+
+export default config;
