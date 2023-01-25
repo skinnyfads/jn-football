109ded663f2fa2d21da88a37d24af629c6eee784
skinnyfads <skinnyfads@proton.me>
2023-01-26 05:05:27 +0700

    feat(api): does not require team score on match schema
    The reason behind this is because if the two teams haven't played yet, we can give their score undefined or even don't need to give them a score at all
    
    This is only to make it easier for the maintaner when querying match data and distinguishing between matches that have not been played and matches that have been played

--- src/models/Match.ts
+++ src/models/Match.ts
@@ -6,8 +6,8 @@ const matchSchema = new mongoose.Schema({
   league: { type: ObjectId, ref: "league", required: true },
   homeTeamName: { type: String, required: true },
   awayTeamName: { type: String, required: true },
-  homeTeamScore: { type: Number, required: true },
-  awayTeamScore: { type: Number, required: true },
+  homeTeamScore: { type: Number },
+  awayTeamScore: { type: Number },
   done: { type: Boolean, required: true },
 });
 const Match = mongoose.model("match", matchSchema);
